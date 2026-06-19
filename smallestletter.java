7f0b729eea846f17a698d695f557a2b8396abc5b
saikiran <saikiranbehra999@gmail.com>
2026-06-19 23:02:59 +0530

    added new problem

---
+++ smallestletter.java
@@ -0,0 +1,13 @@
+
+public class smallestletter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int ans=1%10;
+  // for(int x:Integer.toIntArray)
+System.out.println(ans);	
+}
+
+	
+
+}
